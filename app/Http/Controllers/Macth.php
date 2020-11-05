da09ea2c203a5174ec3e60721122bffa9c3c1e69
yuan putra <bhalayuan@gmail.com>
2020-11-05 15:05:41 +0700

    d

--- app/Http/Controllers/Macth.php
+++ app/Http/Controllers/Macth.php
@@ -76,8 +76,8 @@ class Macth extends Controller
     public function air_minum($tahun){
     	$d=DB::table('public.master_daerah as d')
     	->selectRaw("d.id,min(d.nama) as nama, count(k.*) jumlah_kegiatan")
-    	->leftJoin('rkpd.master_'.$tahun.'_kegiatan as k',[['k.kodepemda','=','d.id']])
-    	->where([['k.id_urusan','=',3],['k.id_sub_urusan','=',12],['d.kode_daerah_parent','=',null]])
+    	->leftJoin('rkpd.master_'.$tahun.'_kegiatan as k',[['k.kodepemda','=','d.id'],['k.id_urusan','=',3],['k.id_sub_urusan','=',12]])
+    	->where([['d.kode_daerah_parent','=',null]])
     	->orderBy('d.id','asc')->groupBy('d.id')->get();
 
     	return view('match_air_minum')->with('data',$d);
